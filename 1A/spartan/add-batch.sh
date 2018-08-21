6f05cd90c72bb04cd7b81bb3487748ebcfcb1ea1
s1855905 <m.vandelft@student.utwente.nl>
2018-08-21 10:55:32 +1000

    Script files.

--- 1A/spartan/add-batch.sh
+++ 1A/spartan/add-batch.sh
@@ -1,3 +1,6 @@
-sbatch 2-cores.slurm
-sbatch 4-cores.slurm
-sbatch 8-cores.slurm
+sbatch slurm-files/2-cores.slurm
+sbatch slurm-files/4-cores.slurm
+sbatch slurm-files/8-cores.slurm
+sbatch slurm-files/12-cores.slurm
+sbatch slurm-files/18-cores.slurm
+sbatch slurm-files/36-cores.slurm
--- 1A/spartan/gather-output.sh
+++ 1A/spartan/gather-output.sh
@@ -1,4 +1,10 @@
-echo execution date: $(date) >> output.txt
-echo "*******" >> output.txt
-echo " " >> output.txt
-cat *.out >> output.txt && rm *.out
+#rm output.txt > /dev/null 2>&1 # hide output
+
+DATE=$(date -I's')
+FILE=results/$DATE.txt
+
+echo execution date: $DATE >> $FILE
+echo "*******" >> $FILE
+echo " " >> $FILE
+cat output/*.out >> $FILE 
+#rm output/*.out
