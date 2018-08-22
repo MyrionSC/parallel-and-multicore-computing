b7ab1482c5c79e551488dfb249e09c4366c753ce
Martin Raunkjaer Andersen (unimelb) <marand@spartan.hpc.unimelb.edu.au>
2018-08-22 15:03:12 +1000

    changed bigmem cores to be power of 2

--- 1A/spartan/add-batch.sh
+++ 1A/spartan/add-batch.sh
@@ -4,5 +4,6 @@ sbatch slurm-files/4-cores.slurm
 sbatch slurm-files/8-cores.slurm
 sbatch slurm-files/12-cores.slurm
 sbatch slurm-files/12-cores-phys.slurm
-sbatch slurm-files/18-cores.slurm
-sbatch slurm-files/36-cores.slurm
+sbatch slurm-files/16-cores.slurm
+sbatch slurm-files/32-cores.slurm
+sbatch slurm-files/64-cores.slurm
