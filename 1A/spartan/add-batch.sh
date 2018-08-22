9fc6747a48e4426c02c748c0a3121a604501afbc
Martin Raunkjaer Andersen (unimelb) <marand@spartan.hpc.unimelb.edu.au>
2018-08-22 13:14:31 +1000

    added 12 cores physical slurm file

--- 1A/spartan/add-batch.sh
+++ 1A/spartan/add-batch.sh
@@ -2,5 +2,6 @@ sbatch slurm-files/2-cores.slurm
 sbatch slurm-files/4-cores.slurm
 sbatch slurm-files/8-cores.slurm
 sbatch slurm-files/12-cores.slurm
+sbatch slurm-files/12-cores-phys.slurm
 sbatch slurm-files/18-cores.slurm
 sbatch slurm-files/36-cores.slurm
