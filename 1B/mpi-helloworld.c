4110afd837365fa955b3b45c69d7a5233340c19e
myrionsc <marpht23@gmail.com>
2018-09-02 11:57:08 +0845

    testing procname in hello world on spartan

--- 1B/mpi-helloworld.c
+++ 1B/mpi-helloworld.c
@@ -7,9 +7,10 @@ char **argv;
 {
     int rank, size;
     MPI_Init( &argc, &argv );
+    char procName[MPI_MAX_PROCESSOR_NAME];
     MPI_Comm_size( MPI_COMM_WORLD, &size );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    printf( "Hello world from process %d of %d\n", rank, size );
+    printf( "%s: Hello world from process %d of %d\n", procName, rank, size );
     MPI_Finalize();
     return 0;
 }
