00a155bfb4ef7353adfca9ef86b39abc17bfa272
myrionsc <marpht23@gmail.com>
2018-09-02 12:05:37 +0845

    hello world mistake

--- 1B/mpi-helloworld.c
+++ 1B/mpi-helloworld.c
@@ -5,11 +5,14 @@ int main( argc, argv )
 int  argc;
 char **argv;
 {
-    int rank, size;
+    int rank, size, nameLenght = 0;
     MPI_Init( &argc, &argv );
     char procName[MPI_MAX_PROCESSOR_NAME];
+
     MPI_Comm_size( MPI_COMM_WORLD, &size );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+    MPI_Get_processor_name(procName, &nameLenght);
+
     printf( "%s: Hello world from process %d of %d\n", procName, rank, size );
     MPI_Finalize();
     return 0;
