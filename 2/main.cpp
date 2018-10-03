dcb143d233bed0178d9e658bdcb3fbb2eb298cf2
Matthijs <Matthijsvandelft@hotmail.com>
2018-10-03 06:46:13 +0200

    Read input from file.

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 #include "omp.h"
 #include "mpi.h"
 
-/* A Naive recursive implementation of 0-1 Knapsack problem */
-#include<stdio.h>
-
-// A utility function that returns maximum of two integers
+/// A utility function that returns maximum of two integers.
 int max(int a, int b) { return (a > b)? a : b; }
 
-// Returns the maximum value that can be put in a knapsack of capacity W
+/// Returns the maximum value that can be put in a knapsack of capacity W.
 int knapSack(int W, int wt[], int val[], int n)
 {
     // Base Case
@@ -29,13 +27,35 @@ int knapSack(int W, int wt[], int val[], int n)
         );
 }
 
-/// Basic knapsack example from: https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
-int main()
+/// Basic knapsack example from: https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/+-
+/// Slightly modified.
+int main(int argc, char *argv[])
 {
-    int val[] = {60, 100, 120};
-    int wt[] = {10, 20, 30};
-    int  W = 50;
-    int n = sizeof(val)/sizeof(val[0]);
-    printf("%d", knapSack(W, wt, val, n));
+    int  W = -1;
+    std::vector<int> values;
+    std::vector<int> weights;
+
+    if(argc != 2){
+        printf("The path to the input file is not specified as a parameter.\n");
+        return -1;
+    }
+
+    FILE *in_file  = fopen(argv[1], "r");
+    if (in_file  == NULL) {
+        printf("Can't open file for reading.\n");
+        return -1;
+    }
+
+    /// Read input file.
+    fscanf(in_file,"%d", &W);
+    int value, weight;
+
+    while(fscanf(in_file,"%d %d", &value, &weight) != EOF) {
+        values.push_back(value);
+        weights.push_back(weight);
+    }
+
+    /// Execute algorithm.
+    printf("%d", knapSack(W, &weights.at(0), &values.at(0), values.size()));
     return 0;
 }
