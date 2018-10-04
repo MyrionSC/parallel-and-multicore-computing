7408e7a72077a4fffe7c53a768790ab5e5780582
myrionsc <marpht23@gmail.com>
2018-10-04 15:19:58 +0845

    created input-generator (takes one arg which is number of nodes generated), and generated some input files

---
+++ 2/input/input-generator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import random
+import sys
+
+if len(sys.argv) > 1:
+    n = int(sys.argv[1])
+else:
+    print("argument 1 must be number of nodes in TSP graph")
+    exit()
+
+adjMatrix = [[0 for y in range(n)] for x in range(n)]
+
+for i in range(0, n):
+    for j in range(0, n):
+        if i != j and adjMatrix[i][j] == 0:
+            adjMatrix[i][j] = adjMatrix[j][i] = random.randint(10, 100)
+
+# for row in adjMatrix:
+#     print(row)
+
+f = open(str(n) + ".txt", "w+")
+f.write(str(n) + "\n")
+for i in range(0, n):
+    for j in range(0, n):
+        f.write(str(i) + " " + str(j) + " " + str(adjMatrix[i][j]) + "\n")
+
+print("data saved to file: " + str(n) + ".txt")
+
